7ca201017d2e58b39334eb4b3d962549d435b785
Gabriel Borel <biel_borel@hotmail.com>
2022-06-22 09:35:20 -0300

    Global css fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Event from './pages/Event';
+
 export default function App() {
-  return <h1 className='text-5xl text-violet-500 font-bold'>Hello World</h1>;
+  return (
+    <div>
+      <Event />
+    </div>
+  );
 }
