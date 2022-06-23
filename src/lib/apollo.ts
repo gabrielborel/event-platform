fc94d9cedfc1c166df58b4afab2b2745de401eb3
Gabriel Borel <biel_borel@hotmail.com>
2022-06-23 10:08:25 -0300

    Removed the cache due to a bug

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, DefaultOptions } from '@apollo/client';
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o62udt0qbv01xig0b27wex/master',
   cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
